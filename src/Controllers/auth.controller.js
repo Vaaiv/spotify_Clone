08323731ebeea6b1b382a00ea412b9c5d961b35d
Vaibhav Yadav <128416054+Vaaiv@users.noreply.github.com>
2026-05-23 23:54:21 +0000

    installed npm's jsonwebtoken module and used its sign function to create token by giving it id of mongodb data and website generated JWTsecret and this whole thing will create the token for the user registered

--- src/Controllers/auth.controller.js
+++ src/Controllers/auth.controller.js
@@ -1,8 +1,29 @@
 const userModel=require('../Models/user.model');
 
+const jwt=require('jsonwebtoken');
+
+
 async function registerUser(req,res){
 
     const {username , email, password}=req.body;
+
+    const user= await userModel.create({
+        username,
+        email,
+        password
+    });
+
+    const token=jwt.sign({ id },
+         process.env.JWT_SECRET,
+          { expiresIn: "7d"});
+
+    res.status(201).json({
+      message:"user created succesfully",
+      _id: user._id,
+      name: user.username,
+      email: user.email,
+      token: token
+    });
 
 };
 
